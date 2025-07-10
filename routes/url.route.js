790419deaff06a63712325c1f64f9cc93bf05bb6
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-07-10 20:38:01 +0200

    first build complete

--- routes/url.route.js
+++ routes/url.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Url = require("../models/Url");
+const Url = require("../models/url.model");
 const { nanoid } = require("nanoid");
 
 // POST - create short URL
