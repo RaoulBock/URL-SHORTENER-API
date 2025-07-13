80958ccd9a21a9ca00a2c5c0ca2bf57bb077c570
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-07-13 15:41:26 +0200

    depebdebcies upgrade

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const urlRoutes = require('./routes/url.route');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const urlRoutes = require("./routes/url.route");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use('/', urlRoutes);
+app.use("/", urlRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-})
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(3000, () =>
+      console.log("Server running on http://localhost:3000")
+    );
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
